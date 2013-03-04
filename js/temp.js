3448915a1fb9cef4ae96e890a5c5e3bc64eafb4d
Jessica David <jessicamdavid@gmail.com>
2013-03-04 12:45:12 -0500

    Added phone audio streams once you click on the desired collective channel.

--- js/temp.js
+++ js/temp.js
@@ -3,8 +3,7 @@ $.ajaxSetup({
 });
         
 var load_gif = "<img src='imgs/ajax_load.gif' alt='Loading streams...' />";
-var sessions = [];
-var streams = {};
+var groups = {};
 
 $(document).ready(function() {
     initializeStreams();
@@ -12,21 +11,39 @@ $(document).ready(function() {
 
 var initializeStreams = function() {
     $('.loading').html(load_gif);
-    $.getJSON('sessions', function(data) {
-        sessions = data["Streams"];
-        var items = [];
-
-        $.each(sessions, function(index, stream) {
-            var id = stream["id"];            
-            var name = stream["name"];
-            streams[id] = stream["phones"];
-            var numPhones = streams[id]["ids"].length
+    $.getJSON('groups', function(data) {
+        $.each(data.Groups, function(index, group) {
+            var id = group.id;            
+            var name = group.name;
+            groups[id] = group.phones;
+            var numPhones = group.phones.ids.length
             $('<div class="stream" id="' + id + '">' +
                 name + ': currently streaming from ' + numPhones +
                 ' device(s).</div>').click(function() {
-                    $(this).fadeOut('slow');
-                }).appendTo("#changethis");
+                    // $(this).fadeOut('slow');
+                    showStreamingPhones(id);
+                    $("h2").fadeOut('slow', function() {
+                        $(this).html("Select a phone's audio stream:").
+                            fadeIn('slow');
+                    });
+                }).appendTo("#streams");
         });
     });
-    $('.loading').hide();
+    $('.loading').remove();
+}
+
+var showStreamingPhones = function(id) {
+    var selectedStream = groups[id];
+    $("#streams").fadeOut('slow', function() {
+        $(this).remove();
+        $.each(selectedStream.ids, function(index, name) {
+            var audio = new Audio("wavs/test.wav");
+            $('<div class="phone" id="' + name + '">' + 
+                '<img src="imgs/phone.png" />' +
+                '<p>Stream #' + (index + 1) + ': ' + name + '</p>' +
+                '</div>').click(function() {
+                    audio.play();
+                }).appendTo("#microphones");
+        });
+    });
 }
